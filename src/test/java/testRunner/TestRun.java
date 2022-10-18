4cca19140b9e1cd25fbb5a96aeb3c4fe4eaf2097
19712 <19712@DESKTOP-HETC9O4>
2022-10-18 01:03:03 -0700

    First Commit

---
+++ src/test/java/testRunner/TestRun.java
@@ -0,0 +1,29 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features={"..\\SampleCucumberPhase2\\Features"},
+		glue="stepDefinitions",
+		dryRun=false,
+		monochrome=true,
+		plugin= {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+			 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"	             
+	}		
+	
+)
+public class TestRun extends AbstractTestNGCucumberTests {
+	@Override
+	@DataProvider(parallel=false)
+	public Object[][] scenarios()
+	{
+		return super.scenarios();
+	}
+
+}
