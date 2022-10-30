7ee499b2a7dd3f2639486da2309f611eb99cb109
19712 <19712@192.168.0.35>
2022-10-29 22:46:58 -0700

    updated to allure.

--- src/test/java/testRunner/testRunner.java
+++ src/test/java/testRunner/testRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",glue ="stepDefinitions"
 ,monochrome=true,
-plugin= {"html:target/cucumber.html",
+plugin= {"pretty","html:target/cucumber.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 })
 public class testRunner {
